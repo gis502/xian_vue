765b56e873675bf805c227badd0a85c1d32f6957
YQ-Bot01 <3411616574@qq.com>
2025-06-28 20:20:53 +0800

    20150628地图高度调整

---
+++ src/views/map/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="cesium-container" ref="cesiumContainer">
+  </div>
+</template>
+
+<script>
+import * as Cesium from 'cesium';
+
+export default {
+  name: 'CesiumRainMap',
+  data() {
+    return {
+      viewer: null,
+      tdtToken: "7f013d0186775b063d6a046977bbefc6",
+    }
+  },
+  computed: {},
+  mounted() {
+    this.load();
+  },
+  beforeDestroy() {
+    if (this.viewer) {
+      this.viewer.destroy();
+      this.viewer = null;
+    }
+  },
+
+  methods: {
+
+    load() {
+      Cesium.Ion.defaultAccessToken = '';
+      const container = this.$refs.cesiumContainer;
+      this.viewer = new Cesium.Viewer(container, {
+        imageryProvider: false,
+        animation: false,
+        homeButton: false,
+        navigationHelpButton: false,
+        timeline: false,
+        selectionIndicator: false,
+        sceneModePicker: false,
+        infoBox: false,
+        geocoder: false,
+        vrButton: false,
+        fullscreenButton: false,
+        baseLayerPicker: false,
+      });
+
+      this.viewer.cesiumWidget.creditContainer.style.display = "none";
+      this.loadTDT();
+      this.viewer.camera.setView({
+        destination: Cesium.Cartesian3.fromDegrees(108.93, 34.27, 1000000),
+        orientation: {
+          heading: Cesium.Math.toRadians(0),
+          pitch: Cesium.Math.toRadians(-90),
+          roll: 0.0
+        }
+      });
+
+    },
+    // 加载天地图
+    loadTDT(type) {
+      let layerProvider=new Cesium.WebMapTileServiceImageryProvider({
+        url: `http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=${this.tdtToken}`,
+        layer: "tdtBasicLayer",
+        style: "default",
+        format: "image/jpeg",
+        tileMatrixSetID: "GoogleMapsCompatible"
+      })
+      const tdtCvaProvider = new Cesium.WebMapTileServiceImageryProvider({
+        url: `http://{s}.tianditu.gov.cn/cva_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=cva&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=${this.tdtToken}`,
+        layer: "cva",
+        style: "default",
+        format: "tiles",
+        tileMatrixSetID: "GoogleMapsCompatible",
+        subdomains: ['t0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'],
+        tileMatrixLabels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'],
+      });
+
+      // viewer.imageryLayers.get(0).show = false;
+      // let layerProvider = new Cesium.UrlTemplateImageryProvider({
+      //   url: 'https://10.22.245.226:8889/kgis/rest/services/GETileMercatorNew/MapServer/tile/{z}/{y}/{x}',
+      //   fileExtension: 'png',
+      //
+      // })
+      this.viewer.imageryLayers.addImageryProvider(layerProvider)
+      this.viewer.imageryLayers.addImageryProvider(tdtCvaProvider)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cesium-container {
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  overflow: hidden;
+}
+</style>
