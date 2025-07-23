4c05650af062863547ae1df1033f1d3402c6d838
Qiuhuawa <1010565096@qq.com>
2025-07-23 22:49:44 +0800

    c c1

--- src/cesium/initLayer.js
+++ src/cesium/initLayer.js
@@ -1,6 +1,6 @@
 import * as Cesium from "cesium";
 const CesiumIonDefaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1ZDBjZjAxOS0wMDhhLTRmZjEtYjNmOC1iNmM2ZmY2ZmQ1N2IiLCJpZCI6MjAxMDI1LCJpYXQiOjE3MTAxNTgxNjJ9.mdbJYEzXQkBnHNqpozz7MvZjJ_X9a3JZRGPA-ytGhLI'
-const tdtToken = "fc6cb1139b8eed4f79439130eb34eb00"
+const tdtToken = "07f071d2d20098468ee7697112e8fc58"
 
 export function initCesium(container) {
     // 使用Cesium官方示例中的Token
