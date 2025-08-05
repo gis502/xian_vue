5b8273435bddb1e986cc5c41d7f72c1d14af6283
Smile <3039126061@qq.com>
2025-08-05 16:40:54 +0800

    20250730知识图谱新闻页面固定标题和每条新闻不换行展示

--- src/views/graph/index.vue
+++ src/views/graph/index.vue
@@ -59,7 +59,7 @@
             <thead>
             <tr>
               <th style="width: 50px">序号</th>
-              <th style="width: 80px">来源</th>
+              <th style="width: 130px">来源</th>
               <th style="width: 120px">标题</th>
               <th style="width: 160px">时间</th>
               <th style="width: 80px">发布者</th>
