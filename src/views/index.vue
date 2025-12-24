7025e7c8b3eff998282379ef0584fd38852b6905
Qiuhuawa <1010565096@qq.com>
2025-12-24 20:02:06 +0800

    12-12-显示logo

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,7 +2,7 @@
   <div class="index-container">
     <!-- 顶部导航栏 -->
     <nav class="top-nav">
-      <img :src="logo" alt="Logo" style="height: 50px;margin-right: 100px; opacity: 0;" />
+      <img :src="logo" alt="Logo" style="height: 50px;margin-right: 100px;" />
       <div class="nav-btn-group">
         <!-- 暴雨主菜单：基于Pinia状态激活 -->
         <RouterLink 
